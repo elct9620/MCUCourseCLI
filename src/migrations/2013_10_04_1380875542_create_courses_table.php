44ab7357313a0eabf54fbbb7d8022093274a76b4
蒼時弦也 <elct9620@frost.tw>
2013-10-04 16:52:37 +0800

    Update# add course table

---
+++ src/migrations/2013_10_04_1380875542_create_courses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use MCUCourseCLI\Migration;
+
+class CreateCoursesTable extends Migration {
+  public function up() {
+    $this->shcema()->create('courses', function($table)
+    {
+      $table->increments('id');
+      $table->integer('system');
+      $table->string('course_code');
+      $table->string('course_name');
+      $table->string('class_code')->unique();
+      $table->integer('max_people');
+      $table->integer('selected_people');
+      $table->string('teacher');
+      $table->string('time');
+      $table->integer('year');
+      $table->string('class_room');
+      $table->integer('camps');
+      $table->integer('type');
+      $table->integer('credit');
+      $table->integer('type');
+      $table->integer('semester');
+      $table->timestamps();
+    });
+  }
+
+  public function down() {
+    $this->schema()->drop('courses');
+  }
+}
